17d55bae0b807a1865dfc21a8c9333d9f6c76cef
ryukkn <126628758+ryukkn@users.noreply.github.com>
2024-08-12 02:44:22 +0800

    kenB: quiz template

--- src/app/dashboard/users/quiz/quiz.module.ts
+++ src/app/dashboard/users/quiz/quiz.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { QuizRoutingModule } from './quiz-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    QuizRoutingModule
+    QuizRoutingModule,
+    FormsModule
   ]
 })
 export class QuizModule { }
