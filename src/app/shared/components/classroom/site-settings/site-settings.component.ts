16e91371f02e62b9208e87176708c167a0b1e486
Acon1tum <cj.concepcion7@gmail.com>
2024-08-12 15:48:04 +0800

    site settings

--- src/app/shared/components/classroom/site-settings/site-settings.component.ts
+++ src/app/shared/components/classroom/site-settings/site-settings.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-site-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './site-settings.component.html',
-  styleUrl: './site-settings.component.scss'
+  styleUrls: ['./site-settings.component.scss']
 })
-export class SiteSettingsComponent {
+export class SiteSettingsComponent implements OnInit {
+  currentSection: string = 'emailConfig';
 
+  ngOnInit() {
+    this.showSection(this.currentSection);
+  }
+
+  showSection(section: string) {
+    this.currentSection = section;
+  }
 }
