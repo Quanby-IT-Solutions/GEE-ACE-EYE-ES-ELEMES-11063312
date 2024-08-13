8c5b73660d2e15ed210d4ab4349ba36bc0f26995
cleomark <cleocalitisin@gmail.com>
2024-08-13 20:41:01 +0800

    create new component for user approval

---
+++ src/app/shared/components/classroom/user-management/user-approval/new-user-approval/new-user-approval.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-user-approval',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-user-approval.component.html',
+  styleUrl: './new-user-approval.component.scss'
+})
+export class NewUserApprovalComponent {
+
+}
